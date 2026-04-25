9ea8dd9a7609ca769b999855c3ff0c5386db68df
fan571 <2582385686@qq.com>
2026-04-25 15:55:25 +0800

    Delete 途遇 directory

--- 途遇/js/survey.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const surveyForm = document.querySelector('.survey-container form');
-    if (surveyForm) {
-        surveyForm.addEventListener('submit', function(e) {
-            e.preventDefault();
-            alert('感谢您的参与！问卷已提交。');
-        });
-    }
-});
